e78b131a60eaabf296f0134704f7b48497498fab
VitoJan <56464847+vitojan@users.noreply.github.com>
2023-06-21 12:00:41 +0800

    update

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import os
 import tkinter as tk
 from tkinter import filedialog, messagebox, StringVar
-
+from PIL import Image, ImageTk
 
 # 建立表情包存放目錄，若已存在則跳過
 if not os.path.exists("emojis"):
@@ -22,34 +22,41 @@ def add_emoji():
             print(new_file_name)
             os.system(("copy "+file_path+" "+new_file_name).replace("/", "\\"))
             messagebox.showinfo("新增表情包", "表情包新增成功！")
+            
+# 定義函式：選擇顯示
+def select(event):
+    widget = event.widget
+    selection=widget.curselection()
+    if widget.curselection():
+        picked = widget.get(selection[0])
+        image = Image.open("emojis\\"+picked)
+        zoomy = int(image.size[1]/(image.size[0]/160))
+        image = image.resize((160,zoomy))
+        img = ImageTk.PhotoImage(image)
+        labelimage.place(x=292, y=220-int(zoomy/2))
+        labelimage.configure(image=img)
+        labelimage.image = img
 
-# 定義函式：搜尋表情包
-def search_emoji():
-    # 取得搜尋關鍵字
-    keyword = keyword_entry.get()
-    # 搜尋表情包目錄下的所有檔案
-    result = []
-    for file_name in os.listdir("emojis"):
-        # 若檔案名稱中包含關鍵字，則加入搜尋結果
-        if keyword.lower() in file_name.lower():
-            result.append(file_name)
-    # 顯示搜尋結果
-    if result:
-        result_label.config(text="\n".join(result))
-    else:
-        result_label.config(text="查無結果！")
+
 
 # 建立主視窗
 window = tk.Tk()
 window.title("表情包管理軟體")
+window.iconbitmap("ico.ico")
+window.resizable(width=False, height=False)
+window.minsize(width=480, height=320)
+window.configure(bg="#DDDDFF")
+
+labelimage = tk.Label(window,bg="#DDDDFF")
+labelimage.place(x=292, y=320)
 
 # 建立元件：新增表情包按鈕
-add_button = tk.Button(window, text="新增表情包", command=add_emoji)
-add_button.pack(padx=20, pady=10)
+add_button = tk.Button(window, text="新增表情包", command=add_emoji, font=("Arial", 10), width=8, height=4, bg="#D0E0EE")
+add_button.place(x=340, y=40)
 
 # 建立元件：搜尋表情包標籤
-search_label = tk.Label(window, text="搜尋表情包", font=("Arial", 16))
-search_label.pack(padx=20, pady=10)
+search_label = tk.Label(window, text="搜尋表情包", font=("Arial", 16),bg="#DDDDFF")
+search_label.place(x=90, y=30)
 
 # 建立元件：搜尋表情包輸入框
 
@@ -57,22 +64,16 @@ sv = StringVar()
 sv.trace("w", lambda name, index, mode, sv=sv: on_keyword_changed(sv))
 
 keyword_entry = tk.Entry(window, font=("Arial", 14), textvariable=sv)
-keyword_entry.pack(padx=20, pady=5)
+keyword_entry.place(x=35, y=80)
 
-# 建立元件：搜尋表情包按鈕
-search_button = tk.Button(window, text="搜尋", font=("Arial", 14), command=search_emoji)
-search_button.pack(padx=20, pady=5)
-
-# 建立元件：搜尋結果標籤
-result_label = tk.Label(window, text="", font=("Arial", 12), wraplength=600)
-result_label.pack(padx=20, pady=5)
 #建立元件：滾動條
 scrollbar = tk.Scrollbar(window)
 scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
 
 #建立元件：搜尋結果列表
-result_listbox = tk.Listbox(window, yscrollcommand=scrollbar.set, font=("Arial", 12), width=50, height=10)
-result_listbox.pack(padx=20, pady=10)
+result_listbox = tk.Listbox(window, yscrollcommand=scrollbar.set, font=("Arial", 12), width=30, height=10)
+result_listbox.place(x=10, y=125)
+result_listbox.bind("<<ListboxSelect>>",select)
 
 #設定滾動條與列表的連動
 scrollbar.config(command=result_listbox.yview)
